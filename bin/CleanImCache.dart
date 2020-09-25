c754438dc9b415416a7a4e320c960591488a34ae
wjw465150 <wjw465150@gmail.com>
2020-09-25 10:57:31 +0800

    fix: CleanImCache will break when IO Exception!

--- bin/CleanImCache.dart
+++ bin/CleanImCache.dart
@@ -4,13 +4,18 @@ const rootPath = r'c:/WJW_D/个人配置/My Documents/';
 //各种IM的缓存目录
 List<String> _cachePath = [
   '${rootPath}Tencent Files/746861174/Image/Group2',
+  '${rootPath}Tencent Files/746861174/Image/C2C',
   '${rootPath}Tencent Files/746861174/Video',
   '${rootPath}Tencent Files/746861174/FileRecv',
   '${rootPath}Tencent Files/746861174/Audio',
   '${rootPath}WXWork/1688853573303163/Cache/File',
   '${rootPath}WXWork/1688853573303163/Cache/Image',
   '${rootPath}WXWork/1688853573303163/Cache/Video',
-  '${rootPath}WXWork/1688853573303163/Cache/Voice'
+  '${rootPath}WXWork/1688853573303163/Cache/Voice',
+  '${rootPath}WeChat Files/wxid_ltlq2htfyrzw22/FileStorage/Cache',
+  '${rootPath}WeChat Files/wxid_ltlq2htfyrzw22/FileStorage/File',
+  '${rootPath}WeChat Files/wxid_ltlq2htfyrzw22/FileStorage/Image',
+  '${rootPath}WeChat Files/wxid_ltlq2htfyrzw22/FileStorage/Video'
 ];
 
 void main() {
@@ -35,7 +40,11 @@ void main() {
       for (var fs in llFS) {
         if (fs.statSync().size > 0) {
           print("$fs size: ${fileStat}");
-          darDD.deleteSync(recursive: true);
+          try {
+            darDD.deleteSync(recursive: true);
+          } catch (e) {
+            print("$e");
+          }
           break;
         }
       }
